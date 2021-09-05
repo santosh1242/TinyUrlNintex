88345fe3f2cf0ceb61a13a6b1907d384dd178ace
Santosh Panda <Santosh_Panda3@Dell.com>
2021-09-05 17:47:06 +0800

    Yml File Changed

---
+++ Application/TinyUrlConvertsation.webapi/Controllers/UrlConvertionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TinyUrlConvertsation.webapi.Controllers
+{
+    public class UrlConvertionController : ApiController
+    {
+        public string ConvertMainUrlToTinyUrl(string urlvalue)
+        {
+            try
+            {
+                return "output from UrlConversation";
+            }
+            catch (Exception ex)
+            {
+                // Repack to Http error.
+                var httpError = new HttpResponseMessage()
+                {
+                    StatusCode = (HttpStatusCode)422,
+                    ReasonPhrase = ex.Message
+                };
+
+                throw new HttpResponseException(httpError);
+            }
+        }
+    }
+}
